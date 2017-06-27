54406ca080a1b4490c491aff5fac33d5b0e117ee
Ryan Lapeyre <rlapeyre@shelterinsurance.com>
2017-06-27 10:58:19 -0500

    added text document

--- TextEditor/MainWindow.xaml.cs
+++ TextEditor/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,5 +25,63 @@ namespace TextEditor
         {
             InitializeComponent();
         }
+
+        private void OpenAction(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = false;
+            openFileDialog.Filter = "text files (*.txt)|*.txt";
+            openFileDialog.CheckFileExists = true;
+            openFileDialog.CheckPathExists = true;
+            openFileDialog.Title = "Please Select Your File";
+            openFileDialog.ShowDialog();
+            if(!string.IsNullOrEmpty(openFileDialog.FileName))
+            {
+               // openFileDialog.OpenFile();
+            }
+            else
+            {
+                return;
+            }
+            
+        }
+
+        private void SaveAsAction(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "text files (*.txt)|*.txt";
+            saveFileDialog.CheckFileExists = true;
+            saveFileDialog.CheckPathExists = true;
+            saveFileDialog.Title = "Please Select Your File To Save Over";
+            saveFileDialog.ShowDialog();
+            if (!string.IsNullOrEmpty(saveFileDialog.FileName))
+            {
+                // saveFileDialog.OpenFile();
+            }
+            else
+            {
+                return;
+            }
+
+        }
+
+
+        private void CloseCommandHandler(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
+
+        }
+
+        private void MaximizedScreenHandler(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized;
+                return;
+            }
+            this.WindowState = WindowState.Normal;
+            return;
+        }
+
     }
 }
